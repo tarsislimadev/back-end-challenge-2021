d47504a328c0e9a9a4b2f7afc7ca74a7b28b5f08
Tarsis de Lima <br.tmvdl@gmail.com>
2022-03-23 04:01:35 -0300

    Implementing a JSON library

---
+++ app/lib/json/index.js
@@ -0,0 +1,10 @@
+
+module.exports.parse = (str) => {
+  try { return JSON.parse(str) } catch (e) { }
+  return {}
+}
+
+module.exports.stringify = (obj, beautify = false) =>
+  beautify
+    ? JSON.stringify(obj, null, 4)
+    : JSON.stringify(obj)
