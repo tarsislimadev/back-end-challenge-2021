00bdd5ba2d2d7fe7370d7c3a97bc2192a68dba3b
Tarsis de Lima <br.tmvdl@gmail.com>
2022-07-01 22:28:17 -0300

    Adding HTTP lib

---
+++ src/lib/http/request.js
@@ -0,0 +1,17 @@
+function Request({
+  query,
+  route,
+  headers,
+  method,
+  path,
+  body,
+}) {
+  this.query = query
+  this.route = route
+  this.headers = headers
+  this.method = method
+  this.path = path
+  this.body = body
+}
+
+module.exports = Request
